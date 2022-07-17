183614dc2859893a2d7613aaae2fa4121e6a88e5
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-07-17 17:04:34 +0300

    create a function that will do the calculations

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+function productionReport(){
+    Event.preventDefault();
+    let data=[];
+    data[0]=data["shed_A"]=parseInt(document.getElementById("shed_A").value)
+    data[1]=data["shed_B"]=parseInt(document.getElementById("shed_B").value)
+    data[2]=data["shed_C"]=parseInt(document.getElementById("shed_C").value)
+    data[3]=data["shed_D"]=parseInt(document.getElementById("shed_D").value)
+
+    let sumData=data.shed_A + data.shed_B + data.shed_C + data.shed_D;
+    document.getElementById("data2").innerHTML +="<Br>"
+
+    document.getElementById("data2").innerHTML +="<p> Your production in shed A is " + data.shed_A + " litres per day</p>";
+    document.getElementById("data2").innerHTML +="<p> Your production in shed B is " + data.shed_B + " litres per day</p>";
+    document.getElementById("data2").innerHTML +="<p> Your production in shed C is " + data.shed_C + " litres per day</p>";
+    document.getElementById("data2").innerHTML +="<p> Your production in shed D is " + data.shed_D + " litres per day</p>";
+
+    document.getElementById("data2").innerHTML +="Your total production per day is "+ sumData + " litres per day";
+
+
+    dailyIncome = sumData * 45;
+
+    document.getElementById("data2").innerHTML +="<Br>"
+    document.getElementById("data2").innerHTML +="<hr>"
+    document.getElementById("data2").innerHTML +="<p> your total daily income is ksh. " + dailyIncome + " </p>";
+    document.getElementById("data2").innerHTML +="<hr>"
+
+    weeklyIncome= dailyIncome * 7;
+    document.getElementById("data2").innerHTML+="<p> Your total weekly income is ksh." + weeklyIncome + " </p>"
+    document.getElementById("data2").innerHTML +="<hr>"
+    document.getElementById("data2").innerHTML +="<Br>"
+
+
+}
